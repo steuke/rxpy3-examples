337c72ff5681c5cd467e983e3f454ceb20df21b5
Stefan Ukena <stefan@ukena.de>
2021-11-22 18:18:38 +0100

    Adds two examples

---
+++ examples/rx3_buffer_example.py
@@ -0,0 +1,36 @@
+#
+# I have written a short blog-post with some explanation:
+# https://blog.ukena.de/posts/2021/11/rxpy3-multiple-using-observers-and-buffer/
+#
+
+from time import sleep
+
+from rx3 import operators, interval
+
+
+def buffer_example():
+    buffer_marker = interval(1.0).pipe(
+        operators.do_action(
+            on_next=lambda x: print(f"buffer marker {x}"),
+        ),
+    )
+
+    source = interval(0.3).pipe(
+        operators.do_action(
+            on_next=lambda single_item: print(f"action before buffering {single_item}"),
+        ),
+        operators.buffer(buffer_marker),
+        operators.do_action(
+            on_next=lambda buffered_items: print(
+                f"action after buffering {buffered_items}"
+            ),
+        ),
+    )
+    print("Subscribing to source")
+    source.subscribe()
+    while True:
+        sleep(10)
+
+
+if __name__ == "__main__":
+    buffer_example()
---
+++ examples/rx3_multiple_observers_example.py
@@ -0,0 +1,38 @@
+#
+# I have written a a short blog-post with some explanation:
+# https://blog.ukena.de/posts/2021/11/rxpy3-multiple-using-observers-and-buffer/
+#
+
+
+from time import sleep
+from typing import Optional
+
+from rx3 import operators, create
+from rx3.core.typing import Observer, Scheduler, Disposable
+
+
+def multiple_observers_example():
+    def dequeue(observer: Observer, scheduler: Optional[Scheduler]) -> Disposable:
+        next_item = 0
+        while True:
+            try:
+                next_item += 1
+                observer.on_next(next_item)
+                sleep(0.3)
+            except Exception as e:
+                observer.on_error(e)
+                return Disposable()
+
+    observable = create(dequeue).pipe(operators.publish())
+    skip4 = observable.pipe(operators.filter(lambda x: x % 3 == 0))
+    skip4.subscribe(lambda x: print(f"skipped to {x}"))
+    buffer = observable.pipe(operators.buffer(skip4))
+    buffer.subscribe(lambda x: print(f"processing buffered events {x}"))
+    print("Connecting")
+    observable.connect()
+    while True:
+        sleep(10)
+
+
+if __name__ == "__main__":
+    multiple_observers_example()
