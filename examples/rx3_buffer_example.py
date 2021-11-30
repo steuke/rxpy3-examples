4328f5b3ba850801e3be0343b9ee796644afc635
Stefan Ukena <stefan@ukena.de>
2021-11-30 10:45:48 +0100

    Implements on_error and on_completed (though they are never called)

--- examples/rx3_buffer_example.py
+++ examples/rx3_buffer_example.py
@@ -12,18 +12,24 @@ def buffer_example():
     buffer_marker = interval(1.0).pipe(
         operators.do_action(
             on_next=lambda x: print(f"buffer marker {x}"),
+            on_completed=lambda: print("buffer marker completed"),
+            on_error=lambda e: print(f"buffer marker error {e}"),
         ),
     )
 
     source = interval(0.3).pipe(
         operators.do_action(
             on_next=lambda single_item: print(f"action before buffering {single_item}"),
+            on_completed=lambda: print("source completed"),
+            on_error=lambda e: print(f"source error {e}"),
         ),
         operators.buffer(buffer_marker),
         operators.do_action(
             on_next=lambda buffered_items: print(
                 f"action after buffering {buffered_items}"
             ),
+            on_completed=lambda: print("buffer completed"),
+            on_error=lambda e: print(f"buffer error {e}"),
         ),
     )
     print("Subscribing to source")
