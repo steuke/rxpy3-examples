95f26d8d11e05044fb85c7d8fa83a04f3babf4cd
Stefan Ukena <stefan@ukena.de>
2021-11-30 10:11:50 +0100

    Removes sleep() in favor of list of items

--- examples/rx3_rolling_buffer_example.py
+++ examples/rx3_rolling_buffer_example.py
@@ -3,26 +3,28 @@
 # https://blog.ukena.de/posts/2021/11/rxpy3-sliding-or-rolling-buffer-operator/
 #
 
-from time import sleep
+from typing import Iterable
 
-from rx import operators, interval
+from rx import operators, from_
 
 
-def rolling_buffer_example(buffer_size: int):
-
-    source = interval(0.3).pipe(
+def rolling_buffer_example(items: Iterable, buffer_size: int):
+    source = from_(items).pipe(
         operators.do_action(
             on_next=lambda single_item: print(f"action before buffering {single_item}"),
+            on_completed=lambda: print("source completed"),
+            on_error=lambda e: print(f"source error {e}"),
         ),
         operators.buffer_with_count(buffer_size, 1),
         operators.do_action(
             on_next=lambda buffered_items: print(f"action on buffer {buffered_items}"),
+            on_completed=lambda: print("buffer completed"),
+            on_error=lambda e: print(f"buffer error {e}"),
         ),
     )
     print("Subscribing to source")
     source.subscribe()
-    sleep(2)
 
 
 if __name__ == "__main__":
-    rolling_buffer_example(3)
+    rolling_buffer_example(items=range(0, 6), buffer_size=3)
